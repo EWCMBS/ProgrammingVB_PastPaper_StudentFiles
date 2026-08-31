c3d151a501767ebc6fb8e1a3652d9fd2ac0c148d
EWCMBS <mlungisim@ewc.edu.za>
2026-08-31 20:57:31 +0200

    Extra Student Files

---
+++ 2022_isd/Introduction to Systems Development L2 P2/computeNumbers/computeNumbers/computeNumbers/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtFirst = New System.Windows.Forms.TextBox
+        Me.txtSecond = New System.Windows.Forms.TextBox
+        Me.lstDisplay = New System.Windows.Forms.ListBox
+        Me.btnCompute = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.btnExit = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Second number"
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(107, 50)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirst.TabIndex = 2
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(107, 88)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(100, 20)
+        Me.txtSecond.TabIndex = 3
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(292, 50)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(159, 147)
+        Me.lstDisplay.TabIndex = 4
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(12, 174)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompute.TabIndex = 5
+        Me.btnCompute.Text = "Compute"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(107, 174)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(200, 174)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 264)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Examination number"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtFirst As System.Windows.Forms.TextBox
+    Friend WithEvents txtSecond As System.Windows.Forms.TextBox
+    Friend WithEvents lstDisplay As System.Windows.Forms.ListBox
+    Friend WithEvents btnCompute As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ 2022_isd/Introduction to Systems Development L2 P2/computeNumbers/computeNumbers/computeNumbers/Form1.vb
@@ -0,0 +1,35 @@
+﻿Option Strict On
+Option Explicit On
+Public Class Form1
+    Dim intFirstNumber As Integer
+    Dim intSecondNumber As Integer
+    Dim intHighestNumber As Integer
+    Dim decDivision As Decimal
+    Dim intRemainder As Integer
+    Dim decAverage As Decimal
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lstDisplay.Items.Add("Summary calculations")
+        lstDisplay.Items.Add("---------------------------------------------------")
+        lstDisplay.Items.Add("")
+    End Sub
+
+    Private Sub btnCompute_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCompute.Click
+          
+    End Sub
+   
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        Dim i As Integer
+
+        For i = 3 To lstDisplay.Items.Count - 1
+            lstDisplay.Items.RemoveAt(lstDisplay.Items.Count - 1)
+        Next
+      
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        If MessageBox.Show("Do you want to exit the program", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+
+        End If
+    End Sub
+End Class
